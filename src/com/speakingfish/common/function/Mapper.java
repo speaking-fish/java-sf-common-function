3985d0b31b7f0c50be2c8590ee842f2fbaa05518
borka <borka@localhost>
2016-05-10 00:02:39 +0300

    fix

--- src/com/speakingfish/common/function/Mapper.java
+++ src/com/speakingfish/common/function/Mapper.java
@@ -2,6 +2,7 @@ package com.speakingfish.common.function;
 
 import java.util.function.Function;
 
+@FunctionalInterface
 public interface Mapper<RESULT, SOURCE> extends Function<SOURCE, RESULT> {
     
 }
